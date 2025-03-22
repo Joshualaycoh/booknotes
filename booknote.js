1c0e7c357707db42305f86e0802beeb8232e50ee
Joshualaycoh <your_email@example.com>
2025-03-22 19:45:31 +0800

    change env and pg client details

--- booknote.js
+++ booknote.js
@@ -16,11 +16,7 @@ const saltRounds = 12;
 env.config();
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 db.connect();
