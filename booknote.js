cd3252171e6efb9d0d0f49d10a8f5c6612f1f1ed
Joshualaycoh <your_email@example.com>
2025-03-22 20:11:55 +0800

    change  callbackURL: 'https://booknotes-mhqt.onrender.com/auth/google/index',

--- booknote.js
+++ booknote.js
@@ -352,7 +352,7 @@ passport.use(
 passport.use("google", new GoogleStrategy({
  clientID: process.env.GOOGLE_CLIENT_ID,
  clientSecret:process.env.GOOGLE_CLIENT_SECRET,
- callbackURL: "http://localhost:3000/auth/google/index",
+ callbackURL: "https://booknotes-mhqt.onrender.com/auth/google/index",
  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
 }, async (accessToken, refreshToken, profile, cb) => {
   try {
