30962ae91dcdf7df8d76e37acfb8ed38850d6e4c
Joshualaycoh <your_email@example.com>
2025-03-23 13:05:45 +0800

    add console to passport to see problem cause

--- booknote.js
+++ booknote.js
@@ -349,6 +349,9 @@ passport.use(
 })
 );
 
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+
 passport.use("google", new GoogleStrategy({
  clientID: process.env.GOOGLE_CLIENT_ID,
  clientSecret:process.env.GOOGLE_CLIENT_SECRET,
